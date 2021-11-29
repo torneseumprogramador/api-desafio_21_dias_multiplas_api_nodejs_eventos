ccddd7cc388c7b60874ff0895e76452194e4d183
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-11-29 06:05:52 -0300

    versionando conexão com mysql via sequelize

--- src/controllers/eventos_controller.js
+++ src/controllers/eventos_controller.js
@@ -1,7 +1,8 @@
 const Evento = require('../models/evento')
+const sequelize = require("../config/database")
 
 module.exports = class EventosController{
-  static index(req, res, next) {
+  static async index(req, res, next) {
     res.status(200).send([
       new Evento(),
       new Evento(),
