926cb867d50fa1824368322bb7cc81f8f30f944c
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-11-30 06:28:38 -0300

    basic api events

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,5 +5,9 @@ const EventosController = require('../controllers/eventos_controller');
 
 router.get('/', HomeController.index);
 router.get('/eventos.json', EventosController.index);
+router.get('/eventos/:id.json', EventosController.get);
+router.post('/eventos.json', EventosController.create);
+router.put('/eventos/:id.json', EventosController.update);
+router.delete('/eventos/:id.json', EventosController.delete);
 
 module.exports = router;
--- src/controllers/eventos_controller.js
+++ src/controllers/eventos_controller.js
@@ -5,5 +5,68 @@ module.exports = class EventosController{
     let eventos = await Evento.findAll();
     res.status(200).send(eventos);
   }
-}
 
+  static async get(req, res, next) {
+    const id = req.params.id;
+    let evento = await Evento.findOne({ where: {id: id} });
+    res.status(200).send(evento);
+  }
+
+  static async create(req, res, next) {
+    const {id_usuario, titulo, descricao, data_inicio, data_fim} = req.body
+
+    if(!id_usuario || !titulo || !data_inicio || !data_fim){
+      return res.status(400).send({
+        mensagem: "Os campos (id_usuario, titulo, data_inicio, data_fim) são obrigatórios"
+      });
+    }
+
+    let evento = await Evento.create({ 
+      idUsuario: id_usuario,
+      titulo: titulo,
+      descricao: descricao,
+      dataInicio: data_inicio,
+      dataFim: data_fim
+    });
+
+    res.status(201).send(evento);
+  }
+
+  static async update(req, res, next) {
+    const id = req.params.id;
+    const {id_usuario, titulo, descricao, data_inicio, data_fim} = req.body
+
+    if(!id_usuario || !titulo || !data_inicio || !data_fim){
+      return res.status(400).send({
+        mensagem: "Os campos (id_usuario, titulo, data_inicio, data_fim) são obrigatórios"
+      });
+    }
+
+    let evento_base = await Evento.findByPk(id);
+    if(!evento_base) return res.status(404).send({
+      mensagem: "O id de evento enviado não existe"
+    });
+
+    let evento = await evento_base.update({ 
+      idUsuario: id_usuario,
+      titulo: titulo,
+      descricao: descricao,
+      dataInicio: data_inicio,
+      dataFim: data_fim
+    });
+
+    res.status(200).send(evento);
+  }
+
+  static async delete(req, res, next) {
+    const id = req.params.id;
+
+    let evento_base = await Evento.findByPk(id);
+    if(!evento_base) return res.status(404).send({
+      mensagem: "O id de evento enviado não existe"
+    });
+
+    evento_base.destroy();
+    res.status(204).send("");
+  }
+}
